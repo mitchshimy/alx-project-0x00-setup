02b15ad260f67623fe99cbc883a564384fdd476d
shimy <ochiengmishy2016@gmail.com>
2025-05-28 12:23:22 +0300

    commit

--- alx-project-0x00/interfaces/index.ts
+++ alx-project-0x00/interfaces/index.ts
@@ -4,7 +4,7 @@ export interface PillProps {
 
 interface ButtonProps {
   title: string;
-  size?: "small" | "medium" | "large";
-  shape?: "rounded-sm" | "rounded-md" | "rounded-full";
-  styles?: string;
+  size: "small" | "medium" | "large";
+  shape: "rounded-sm" | "rounded-md" | "rounded-full";
+  styles: string;
 }

3f8ab2e9975360f95f55d0fcfb0daa8c2dc92a43
shimy <ochiengmishy2016@gmail.com>
2025-05-28 12:08:55 +0300

    commit

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface ButtonProps {
+  title: string;
+  styles?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ title, styles = "" }) => {
+  return (
+    <button className={`px-4 py-2 font-semibold text-white bg-blue-600 hover:bg-blue-700 transition ${styles}`}>
+      {title}
+    </button>
+  );
+};
+
+export default Button;
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,7 +1,12 @@
+import Card from "@/components/Card"
+import Button from "@/components/Button"
+
 const Landing: React.FC =  () => {
   return (
     <div>
       <h1 className=" text-xl font-extralight">Landing Page</h1>
+      <Card />
+      <Button title="Click me" />
     </div>
   )
 }

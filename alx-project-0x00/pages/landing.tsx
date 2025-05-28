c539c2c9f2e27b69838df7d3e60edf4fd3ee84bf
shimy <ochiengmishy2016@gmail.com>
2025-05-28 12:25:13 +0300

    commit

--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -9,6 +9,7 @@ const Landing: React.FC =  () => {
       <Button title="Small Rounded-sm" size="small" shape="rounded-sm" />
       <Button title="Medium Rounded-md" size="medium" shape="rounded-md" />
       <Button title="Large Rounded-full" size="large" shape="rounded-full" />
+      <Button title="Large Rounded-lg" size="large" shape="rounded-full" />
     </div>
   )
 }
